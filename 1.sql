ccd863fa1d80d039262a0c35dec9f10cf76c6ca8
John <161700222+AlterDome@users.noreply.github.com>
2025-09-15 07:52:31 +0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -3,23 +3,27 @@ USER 'SYSDBA' PASSWORD 'masterkey'
 PAGE_SIZE 16384
 DEFAULT CHARACTER SET UNICODE_FSS;
 
-CREATE TABLE CUSTOMERS (
-    CUST_ID       INTEGER NOT NULL,
-    CUST_NAME     INTEGER,
-    CUST_UNISITE  INTEGER
+CREATE TABLE SCLAD (
+    id     BIGINT,
+    sklad     INTEGER,
+    adres  INTEGER
 );
 
-CREATE TABLE SMS_DETAIL (
-    ID INTEGER NOT NULL,
-    magazin  VARCHAR(30),
-    adres VARCHAR(50)
-);
+--**********************************
+INSERT INTO SCLAD (id, sklad, adres) VALUES 
+(1, a, moscow )
+(2, b, kazan )
+(3, c, piter )
+(4, d, samara )
 
 
-
-
-
-
+--*************************************
+CREATE VIEW VI (
+    id INTEGER NOT NULL,
+    sklad  VARCHAR(30),
+    adres VARCHAR(50))
+SELECT CAST(adres) FROM SCLAD WHERE (ID=1) GROUP_BY adres 
+                                           -- группирует по адресам
 
 
 
